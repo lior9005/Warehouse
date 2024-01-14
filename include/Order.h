8513cb0f3d781afb0970d8a4552b53276e52e1dc
Lior Sharony <liorsharony@gmail.com>
2024-01-14 19:35:27 +0200

    commit

---
+++ include/Order.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+
+class Volunteer;
+
+enum class OrderStatus {
+    PENDING,
+    COLLECTING,
+    DELIVERING,
+    COMPLETED,
+};
+
+#define NO_VOLUNTEER -1
+
+class Order {
+
+    public:
+        Order(int id, int customerId, int distance);
+        int getId() const;
+        int getCustomerId() const;
+        void setStatus(OrderStatus status);
+        void setCollectorId(int collectorId);
+        void setDriverId(int driverId);
+        int getCollectorId() const;
+        int getDriverId() const;
+        OrderStatus getStatus() const;
+        const string toString() const;
+
+    private:
+        const int id;
+        const int customerId;
+        const int distance;
+        OrderStatus status;
+        int collectorId; //Initialized to NO_VOLUNTEER if no collector has been assigned yet
+        int driverId; //Initialized to NO_VOLUNTEER if no driver has been assigned yet
+};
